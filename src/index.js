1b88fea53ed9d9b944f8354021b617544e5dbb4d
Mohd Shahid Khan <khan42290@gmail.com>
2022-09-15 11:27:36 +0530

    feat

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
-
+import { DataProvider } from './context/DataContext';
+import { AuthProvider } from './context/AuthContext';
+import { BrowserRouter as Router } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+     <AuthProvider>
+        <DataProvider>
+          <App />
+        </DataProvider>
+      </AuthProvider>
+      </Router>
   </React.StrictMode>
 );
 
